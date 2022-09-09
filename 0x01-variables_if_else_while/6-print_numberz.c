54244da261b9537a01384df1718fa149f88a1900
Ishaq <umarishaq26@gmail.com>
2022-09-09 15:13:22 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
