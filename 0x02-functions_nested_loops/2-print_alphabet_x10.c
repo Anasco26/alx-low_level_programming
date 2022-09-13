34b0bf2cfe85bd986630e8dab5434bf2f2e518e2
Ishaq <umarishaq26@gmail.com>
2022-09-13 09:40:29 -0700

    Write a function that prints the sign of a number.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Check holberton
+ * print_alphabet_x10 - Check main.h
  * Description: function uses _putchar function to print
  * alphabet in lowercase 10 times
  * Return: Nothing.
