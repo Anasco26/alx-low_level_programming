af694a0bcff3946073779fe3e0c383a28b39aa36
Ishaq <umarishaq26@gmail.com>
2022-09-14 09:05:38 -0700

    Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * Return: Always return 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
+}
