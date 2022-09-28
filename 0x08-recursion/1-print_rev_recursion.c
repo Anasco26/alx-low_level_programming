57e5891133d304ca49005da5b26603878c2ddb7d
Ishaq <umarishaq26@gmail.com>
2022-09-28 02:31:55 -0700

    Reverse array with recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s++);
+		_putchar(*s);
+	}
+}
