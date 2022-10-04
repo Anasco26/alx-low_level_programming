0fcd6550de451820aa2d74d6cbe7edd2e3ea8552
Ishaq <umarishaq26@gmail.com>
2022-10-04 09:36:06 -0700

    Sum all arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the addition of positive numbers,
+ * followed by a new line.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: If one of the numbers contains symbols that are non-is - 1.
+ * Otherwise - 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int n, i, sum = 0;
+
+	for (n = 1; n < argc; n++)
+	{
+		for (i = 0; argv[n][i]; i++)
+		{
+			if (argv[n][i] < '0' || argv[n][i] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[n]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
