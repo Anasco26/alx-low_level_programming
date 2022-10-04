72961e420ed24fe9674de6cc5f02a5a54a4f002a
Ishaq <umarishaq26@gmail.com>
2022-10-04 08:48:17 -0700

    Function that return its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument array
+ * Return: 0 for success number otherwise
+ */
+
+int main(argc, argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
