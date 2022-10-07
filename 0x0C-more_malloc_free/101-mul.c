8a2570971e74cc6f246a1ef1c1c0e3961b917efd
Ishaq <umarishaq26@gmail.com>
2022-10-06 18:44:55 -0700

    Program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -117,13 +117,10 @@ int main(int argc, char *argv[])
 			_putchar(e[ti]);
 		exit(98);
 	}
-
 	for (l1 = 0; argv[1][l1]; l1++)
 		;
-
 	for (l2 = 0; argv[2][l2]; l2++)
 		;
-
 	ln = l1 + l2 + 1;
 	a = malloc(ln * sizeof(char));
 	if (a == NULL)
@@ -132,7 +129,6 @@ int main(int argc, char *argv[])
 			_putchar(e[ti]);
 		exit(98);
 	}
-
 	init(a, ln - 1);
 	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
 	{
