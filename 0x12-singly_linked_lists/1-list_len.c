0fa276ba137c41ad1c6924dcc67827b9978a4427
Ishaq <umarishaq26@gmail.com>
2022-10-20 10:17:16 -0700

    Node Length error fixed

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * list_len - Function to get list length
+ * @h: Pointer to list head
+ *
+ * Return: Lenght of node
+ */
+
 size_t list_len(const list_t *h)
 {
 	size_t n = 0;
 
-	while(h)
+	while (h)
 	{
 		n++;
 		h = h->next;
