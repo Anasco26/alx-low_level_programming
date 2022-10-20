9881122cf06bdf25d3befc5c11818f88ac2bd1b4
Ishaq <umarishaq26@gmail.com>
2022-10-20 16:26:13 -0700

    Bug fixed for add_list_end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,14 +11,13 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
+	list_t *new, *last;
 	int len = 0;
 
 	new = malloc(sizeof(list_t));
-
 	if (new == NULL)
 		return (NULL);
-	new->str = strdup(str);
+
 	if (str == NULL)
 	{
 		free(new);
@@ -26,18 +25,20 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	while (str[len])
 		len++;
-
+	
+	new->str = strdup(str);
 	new->len = len;
 	new->next = NULL;
-	
-	/* Traverse to the last node */
+
+	/* Traversing through list */
 	if (*head == NULL)
 		*head = new;
-	else
+	else 
 	{
-		while ((*head)->next)
-			*head = (*head)->next;
-		(*head)->next = new;
+		last = *head;
+		while (last->next)
+			last = last->next;
+		last->next = new;
 	}
 
 	return (*head);
