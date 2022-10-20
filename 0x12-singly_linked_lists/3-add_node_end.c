edb5886a75f1f0905bc44e8e52f2de2afc794110
Ishaq <umarishaq26@gmail.com>
2022-10-20 15:58:37 -0700

    Add to last

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,14 +12,14 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
-	int n;
+	int len = 0;
 
 	new = malloc(sizeof(list_t));
 
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
-	if (new->str = NULL);
+	if (str == NULL)
 	{
 		free(new);
 		return (NULL);
@@ -29,10 +29,16 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new->len = len;
 	new->next = NULL;
+	
+	/* Traverse to the last node */
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		while ((*head)->next)
+			*head = (*head)->next;
+		(*head)->next = new;
+	}
 
-	while (*head->next)
-		*head = *head->next;
-	*head = new;
-
-	return (new);
+	return (*head);
 }
