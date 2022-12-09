af410ef1ffeffb3d32dc1470b5b2f7f9fefac1b1
Ishaq <umarishaq26@gmail.com>
2022-12-08 17:08:35 -0800

    Delete double node

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -2,12 +2,12 @@
 
 /**
  * delete_dnodeint_at_index - Deletes a node from a dlistint_t
- *                            at a given index.
+ * at a given index.
  * @head: A pointer to the head of the dlistint_t.
  * @index: The index of the node to delete.
  *
  * Return: Upon success - 1.
- *         Otherwise - -1.
+ * Otherwise - -1.
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
