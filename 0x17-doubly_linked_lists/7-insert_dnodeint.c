f8fb35ee091cc785a7cc5f9fb02a6b0b9510dd20
Ishaq <umarishaq26@gmail.com>
2022-12-08 16:59:07 -0800

    Insert double node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,13 +2,13 @@
 
 /**
  * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
- *                            list at a given position.
+ * list at a given position.
  * @h: A pointer to the head of the dlistint_t list.
  * @idx: The position to insert the new node.
  * @n: The integer for the new node to contain.
  *
  * Return: If the function fails - NULL.
- *         Otherwise - the address of the new node.
+ * Otherwise - the address of the new node.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
