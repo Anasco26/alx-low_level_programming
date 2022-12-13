8cc48b44de301a487a516a1f33857876f1eac54c
Ishaq <umarishaq26@gmail.com>
2022-12-12 19:07:02 -0800

    Dynamic library created

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to get the length of.
+ * Return: The length of @str.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
