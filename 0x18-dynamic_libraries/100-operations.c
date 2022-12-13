c972fa93519c3a8f9d34d83882b87e1fad6ec1d8
Ishaq <umarishaq26@gmail.com>
2022-12-12 19:35:01 -0800

    dynamic lib for python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+
+/**
+ * add - sum two int
+ * @a: First value
+ * @b: Second value
+ *
+ * Return: The sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - difference of two int
+ * @a: first number
+ * @b: second number
+ *
+ * Return: their differences
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - product of two int
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: their product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division of two ints
+ * @a: the frist number
+ * @b: the second number
+ *
+ * Return: integer division value
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - remainder of the result of two ints
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: Their remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
